839a675edf21d772e2bfe27c12398ce3d2692315
apaneser <56094764+apaneser@users.noreply.github.com>
2021-08-31 01:09:14 -0700

    create site

--- scraping.py
+++ scraping.py
@@ -1,12 +1,25 @@
 # import Splinter and BeautifulSoup
+from re import M
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 
 import pandas as pd
+import datetime as dt
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():    
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    data = {
+        'news_title': news_title,
+        'news_paragraph': news_paragraph,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(),
+        'last_modified': dt.datetime.now()
+    }
+    browser.quit()
+    return data
 
 def mars_news(browser):
     # visit the mars nasa news site
@@ -68,7 +81,7 @@ def mars_facts():
 
     return df.to_html()
 
-browser.quit()
-
+if __name__ == '__main__':
+    print(scrape_all())
 
 
